75ab11bdcfb98f7b9494cb6560643cfbfa96550a
Satish.Panjala <Satish.Panjala@elysiumanalytics.ai>
2025-11-19 16:36:54 +0530

    test 8

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,21 +1,11 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-export default function Home() {
-  const [msg, setMsg] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("/api/")
-      .then(res => res.json())
-      .then(data => setMsg(data.message))
-      .catch(() => setMsg("API Error"));
-  }, []);
+export default async function Home() {
+  const res = await fetch("https://test-app-001.vercel.app/backend/items");
+  const data = await res.json();
 
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Next.js + FastAPI</h1>
-      <p>Backend says: {msg}</p>
+    <div>
+      <h1>Welcome Page</h1>
+      <p>Backend says: {data.message}</p>
     </div>
   );
 }
