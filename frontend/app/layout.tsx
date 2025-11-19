5b49bd35d131f370048f70ee948d3c90759376ca
Satish.Panjala <Satish.Panjala@elysiumanalytics.ai>
2025-11-19 16:11:59 +0530

    test 3

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Welcome App",
+  description: "Simple Next.js App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
